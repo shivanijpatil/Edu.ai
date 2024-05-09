a353ba914c342ccaff30b421fd58ed742788f158
shivanijpatil <157980081+shivanijpatil@users.noreply.github.com>
2024-05-09 22:09:31 +0530

    quiz created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import LoginSignup from "./Components/LoginSignup/LoginSignup1";
-
-// import Quiz from "./Components/Quiz/Quiz";
+import Quiz from "./Components/Quiz/Quiz";
 
 function App() {
   return (
     <div>
       <LoginSignup />
-      {/* <Quiz /> */}
+      <Quiz />
     </div>
   );
 }
